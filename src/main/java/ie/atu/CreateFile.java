0e582b26ccb2c01be46eae5c7dc535d404fd49da
Thomas <G00415457@atu.ie>
2023-11-20 16:27:49 +0000

    added CreateFile class and added needed code

--- src/main/java/ie/atu/CreateFile.java
+++ src/main/java/ie/atu/CreateFile.java
@@ -6,6 +6,21 @@ import java.io.IOException;
 public class CreateFile {
     public static void main(String[] args)
     {
-        String fileName = "File13.txt"
+        String fileName = "File13.txt";
+
+        File myFile = new File(fileName);
+
+        try{
+            if(myFile.createNewFile())
+            {
+                System.out.println("File created successfully at " + myFile.getAbsolutePath());
+            }
+            else{
+                System.out.println("File already at  " + myFile.getAbsolutePath());
+            }
+        }catch(IOException e)
+        {
+            System.out.println("An error occured");
+        }
     }
 }
