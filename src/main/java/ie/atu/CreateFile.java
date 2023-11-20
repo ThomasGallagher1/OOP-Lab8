06eb02ffcafc15d9ce1b137dcc55d67a238db76b
Thomas <G00415457@atu.ie>
2023-11-20 16:19:56 +0000

    commited local changes

---
+++ src/main/java/ie/atu/CreateFile.java
@@ -0,0 +1,11 @@
+package ie.atu;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CreateFile {
+    public static void main(String[] args)
+    {
+        String fileName = "File13.txt"
+    }
+}
